3d50319712257be7be9bcad06a2486dde4e7614e
Michael Uvarov <arcusfelis@gmail.com>
2017-09-29 12:45:42 +0200

    Tweak options

--- c_src/vcdiff_nif.cc
+++ c_src/vcdiff_nif.cc
@@ -53,7 +53,8 @@ vcdiff_encode(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[])
 
     std::string output;
     open_vcdiff::VCDiffEncoder encoder((char *)dic.data, dic.size);
-    encoder.SetFormatFlags(open_vcdiff::VCD_FORMAT_INTERLEAVED | open_vcdiff::VCD_FORMAT_CHECKSUM);
+//  encoder.SetFormatFlags(open_vcdiff::VCD_FORMAT_INTERLEAVED | open_vcdiff::VCD_FORMAT_CHECKSUM);
+    encoder.SetTargetMatching(false);
     encoder.Encode((char *)target.data, target.size, &output);
 
     if (!enif_alloc_binary(output.size(), &encoded)) {
